ab297f7511fbcfa57d6d0ef20ea8c0277fd72106
NextepMac <NextepMac@NextepMacs-Mac-mini.local>
2014-12-11 14:59:18 +0400

    Initial Commit

---
+++ calendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calendar
+//
+//  Created by NextepMac on 12/11/14.
+//  Copyright (c) 2014 NextepMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
